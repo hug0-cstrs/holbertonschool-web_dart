cac4a7bc22923614c5be33544c40959c4952efa8
hug0-cstrs <hugo.casteras@outlook.fr>
2024-08-02 19:02:02 +0200

    refactor: Improve longestPalindrome function in Dart

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,3 +1,19 @@
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return "none";
+  }
+  String longest = "";
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 2; j <= s.length; j++) {
+      String sub = s.substring(i, j);
+      if (isPalindrome(sub) && sub.length > longest.length) {
+        longest = sub;
+      }
+    }
+  }
+  return longest.length == 0 ? "none" : longest;
+}
+
 bool isPalindrome(String s) {
   if (s.length < 3) {
     return false;
@@ -7,17 +23,4 @@ bool isPalindrome(String s) {
     reversed += s[i];
   }
   return reversed == s;
-}
-
-String longestPalindrome(String s) {
-  String longest = "";
-  for (int i = 0; i < s.length; i++) {
-    for (int j = i + 1; j <= s.length; j++) {
-      String sub = s.substring(i, j);
-      if (isPalindrome(sub) && sub.length > longest.length) {
-        longest = sub;
-      }
-    }
-  }
-  return longest;
 }
