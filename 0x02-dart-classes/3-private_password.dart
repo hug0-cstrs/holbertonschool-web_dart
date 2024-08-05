cbae83a048db5290302209ea4b3e4d69c0fdb538
hug0-cstrs <hugo.casteras@outlook.fr>
2024-08-05 11:33:32 +0200

    feat: Add Password class with validation and toString method in Dart

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,21 @@
+class Password {
+  String _password = "";
+
+  Password({String password = ""}) {
+    _password = password;
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!_password.contains(RegExp(r'[a-z]'))) return false;
+    if (!_password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
