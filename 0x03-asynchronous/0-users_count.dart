27b0b2c3264e794f2deaed4c0886c11cc003eac8
hug0-cstrs <hugo.casteras@outlook.fr>
2024-08-30 10:10:57 +0200

    refactor: Simplify usersCount function in 0x03-asynchronous/0-users_count.dart

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,10 +1,5 @@
 import '0-util.dart';
 
 Future<void> usersCount() async {
-  try {
-    int count = await fetchUsersCount();
-    print("Number of users: $count");
-  } catch (e) {
-    print("error caught: $e");
-  }
+  print(await fetchUsersCount());
 }
